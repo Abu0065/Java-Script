57fb1bec64ed330d4103d6afda7b99d9b4b41425
Abu0065 <abdullah.azam0065@icloud.com>
2025-11-30 21:37:28 +0530

    Updated files of JS

---
+++ Project1/index.js
@@ -0,0 +1,67 @@
+let items = [];
+
+const itemsDiv = document.getElementById("items")
+const input = document.getElementById("itemInput")
+const storageKey = "items";
+
+function renderItems() {
+    itemsDiv.innerHTML = null;
+
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div")
+        
+        container.className = "todo-item" 
+        
+        const text = document.createElement("p")
+        text.className = "todo-text"
+        text.textContent = item;
+
+        const button = document.createElement("button")
+        button.textContent = "Delete"
+        button.className = "delete-btn" 
+        button.onclick = () => removeItem(idx)
+
+        container.appendChild(text)
+        container.appendChild(button)
+        
+        itemsDiv.appendChild(container)
+    }
+}
+
+function loadItems() {
+    const oldItems = localStorage.getItem(storageKey)
+    if (oldItems) items = JSON.parse(oldItems)
+    renderItems()
+}
+
+function saveItems() {
+    const stringItems = JSON.stringify(items);
+    localStorage.setItem(storageKey, stringItems)
+}
+
+function addItem() {
+    const value = input.value;
+    if (!value) {
+        alert("You cannot add an empty item")
+        return
+    }
+    items.push(value)
+    renderItems()
+    input.value = ""
+    saveItems()
+}
+
+function removeItem(idx) {
+    items.splice(idx, 1)
+    renderItems()
+    saveItems()
+}
+
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addItem();
+    }
+});
+
+document.addEventListener("DOMContentLoaded", loadItems)
